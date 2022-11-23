eced61b13b4c2722d1da80d7a370f695e2f5c84a
jerepon <109601262+jerepon@users.noreply.github.com>
2022-11-23 15:48:12 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './Menu'
+
+function App() {
+  return (
+    <div className="App">
+    <Menu />
+   
+      Holaaaa
+    </div>
+  );
+}
+
+export default App;
---
+++ Menu.js
@@ -0,0 +1,17 @@
+import React from "react";
+export default class Menu extends React.Component{
+	render(){
+		return(
+			<div className="row">
+			<div className="col-12 d-flex">
+			<h1>Artio</h1>
+			<span>Inicio</span>
+			<span>Artistas</span>
+			<span>Contacto</span>
+			</div>
+			</div>)
+	}
+}
+
+	
+
